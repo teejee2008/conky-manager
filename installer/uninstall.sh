1db57c5b846513da328029256682832bca0a3f16
Tony George <tony.george.kol@gmail.com>
2014-07-19 11:43:07 +0530

    updated build scripts

---
+++ installer/uninstall.sh
@@ -0,0 +1,6 @@
+rm -f  "/usr/bin/conky-manager"
+rm -f  "/usr/share/applications/conky-manager.desktop"
+rm -f  "/usr/share/pixmaps/conky-manager.png"
+rm -rf "/usr/share/conky-manager"
+rm -rf "/usr/share/doc/conky-manager"
+rm -f  "/usr/share/locale/*/LC_MESSAGES/conky-manager.mo"
